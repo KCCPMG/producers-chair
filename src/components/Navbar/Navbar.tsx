40cb2f6f643a52acccf7c86662c4f21b9eef5603
Connor Wales <connorwales@gmail.com>
2023-10-06 07:55:03 -0700

    add UserIcon component, work to include it on Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,17 +1,23 @@
 import Link from "next/link";
 import searchIcon from "./../public/iconamoon_search-bold.svg"
+import UserIcon from "../UserIcon/UserIcon";
 
 
 const Navbar = () => {
   return (
-    <nav className="h-6 w-full sticky inline-flex justify-end  bg-black">
-      <Link className="text-white text-xl font-normal font-['Roboto'] capitalize mx-4" href="/">
-        Home
-      </Link>
-      <Link className="text-white text-xl font-normal font-['Roboto'] capitalize mx-4" href="/saved">
-        Saved
-      </Link>     
-      <img className="w-6 h-6 mx-4" alt="search" src="/iconamoon_search-bold.svg" />
+    <nav className="h-14 w-full sticky inline-flex justify-between bg-black px-4">
+      <div>
+        <UserIcon />
+      </div>
+      <div className="inline">
+        <Link className="text-white text-xl font-normal font-['Roboto'] capitalize mx-4" href="/">
+          Home
+        </Link>
+        <Link className="text-white text-xl font-normal font-['Roboto'] capitalize mx-4" href="/saved">
+          Saved
+        </Link>     
+        <img className="w-6 h-6 mx-4" alt="search" src="/iconamoon_search-bold.svg" />
+      </div>
 
     </nav>
   )
---
+++ src/components/UserIcon/UserIcon.tsx
@@ -0,0 +1,11 @@
+
+
+const UserIcon = () => {
+  return (
+    <div className="nav-icon w-[53px] h-[53px] bg-zinc-300 rounded-full">
+      <img />
+    </div>
+  )
+}
+
+export default UserIcon;
