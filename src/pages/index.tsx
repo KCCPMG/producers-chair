20eaaab2b8189a81770dad8c2d4d28d2fec9d1db
Connor Wales <connorwales@gmail.com>
2023-10-12 18:53:05 -0700

    remove original figma code from index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,79 +28,10 @@ export default function Home() {
         <Navbar />
         <HeroImage />
         <Carousel category="Sample" /> 
-        
-
-    <div style={{left: 45, top: 888.80, position: 'absolute', color: 'white', fontSize: 16, fontFamily: 'Roboto', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Top Invested</div>
-    <div style={{left: 45, top: 1200, position: 'absolute', color: 'white', fontSize: 16, fontFamily: 'Roboto', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Features</div>
-    <div style={{left: 157, top: 888.80, position: 'absolute', color: '#A5A5A5', fontSize: 16, fontFamily: 'Roboto', fontWeight: '400', textTransform: 'capitalize', wordWrap: 'break-word'}}>Least invested</div>
-    <div style={{left: 128, top: 1200, position: 'absolute', color: '#A5A5A5', fontSize: 16, fontFamily: 'Roboto', fontWeight: '400', textTransform: 'capitalize', wordWrap: 'break-word'}}>Shorts</div>
-    <div style={{width: 1363, height: 183.80, left: 38, top: 934.80, position: 'absolute'}}>
-
-        <img style={{width: 328.47, height: 183.80, left: 689.69, top: 0, position: 'absolute', borderRadius: 5}} src="https://via.placeholder.com/328x184" />
-        <img style={{width: 328.47, height: 183.80, left: 1034.53, top: 0, position: 'absolute', borderRadius: 5}} src="https://via.placeholder.com/328x184" />
-        <div style={{width: 58.23, height: 35.49, padding: 10, left: 254.77, top: 135.57, position: 'absolute', background: '#1D55A8', borderRadius: 5, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
-            <div style={{color: 'white', fontSize: 16, fontFamily: 'Roboto', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Invest</div>
-        </div>
-        <div style={{width: 58.23, height: 35.49, padding: 10, left: 599.61, top: 135.57, position: 'absolute', background: '#1D55A8', borderRadius: 5, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
-            <div style={{color: 'white', fontSize: 16, fontFamily: 'Roboto', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Invest</div>
-        </div>
-        <div style={{width: 58.23, height: 35.49, padding: 10, left: 944.46, top: 135.57, position: 'absolute', background: '#1D55A8', borderRadius: 5, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
-            <div style={{color: 'white', fontSize: 16, fontFamily: 'Roboto', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Invest</div>
-        </div>
-        <div style={{width: 58.23, height: 35.49, padding: 10, left: 1289.30, top: 135.57, position: 'absolute', background: '#1D55A8', borderRadius: 5, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
-            <div style={{color: 'white', fontSize: 16, fontFamily: 'Roboto', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Invest</div>
-        </div>
-    </div>
-    <div style={{width: 227, height: 0, left: 38, top: 916.80, position: 'absolute', border: '2px #616161 solid'}}></div>
-    <div style={{width: 148, height: 0, left: 38, top: 1228, position: 'absolute', border: '2px #616161 solid'}}></div>
-    <div style={{width: 92, height: 5, left: 45, top: 912.80, position: 'absolute', background: '#A5A5A5'}} />
-    <div style={{width: 48, height: 5, left: 45, top: 1224, position: 'absolute', background: '#A5A5A5'}} />
-    <div style={{width: 1363, height: 183.80, left: 38, top: 1239.59, position: 'absolute'}}>
-        <img style={{width: 328.47, height: 183.80, left: 0, top: 0, position: 'absolute', borderRadius: 5}} src="https://via.placeholder.com/328x184" />
-        <img style={{width: 328.47, height: 183.80, left: 344.84, top: 0, position: 'absolute', borderRadius: 5}} src="https://via.placeholder.com/328x184" />
-        <img style={{width: 328.47, height: 183.80, left: 689.69, top: 0, position: 'absolute', borderRadius: 5}} src="https://via.placeholder.com/328x184" />
-        <img style={{width: 328.47, height: 183.80, left: 1034.53, top: 0, position: 'absolute', borderRadius: 5, border: '2px white solid'}} src="https://via.placeholder.com/328x184" />
-        <div style={{width: 58.23, height: 35.49, padding: 10, left: 254.77, top: 135.57, position: 'absolute', background: '#0375C8', borderRadius: 5, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
-            <div style={{color: 'white', fontSize: 16, fontFamily: 'Roboto', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Invest</div>
-        </div>
-        <div style={{width: 58.23, height: 35.49, padding: 10, left: 599.61, top: 135.57, position: 'absolute', background: '#1D55A8', borderRadius: 5, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
-            <div style={{color: 'white', fontSize: 16, fontFamily: 'Roboto', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Invest</div>
-        </div>
-        <div style={{width: 58.23, height: 35.49, padding: 10, left: 944.46, top: 135.57, position: 'absolute', background: '#1D55A8', borderRadius: 5, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
-            <div style={{color: 'white', fontSize: 16, fontFamily: 'Roboto', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Invest</div>
-        </div>
-        <div style={{width: 58.23, height: 35.49, padding: 10, left: 1289.30, top: 135.57, position: 'absolute', background: '#1D55A8', borderRadius: 5, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
-            <div style={{color: 'white', fontSize: 16, fontFamily: 'Roboto', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Invest</div>
-        </div>
-    </div>
-    <div style={{left: 110, top: 586, position: 'absolute', color: '#A5A5A5', fontSize: 16, fontFamily: 'Roboto', fontWeight: '400', textTransform: 'capitalize', wordWrap: 'break-word'}}>Oldest</div>
-    <div style={{width: 1363, height: 183.80, left: 38, top: 630, position: 'absolute'}}>
-        <img style={{width: 328.47, height: 183.80, left: 0, top: 0, position: 'absolute', borderRadius: 5}} src="https://via.placeholder.com/328x184" />
-        <img style={{width: 328.47, height: 183.80, left: 344.84, top: 0, position: 'absolute', borderRadius: 5}} src="https://via.placeholder.com/328x184" />
-        <img style={{width: 328.47, height: 183.80, left: 689.69, top: 0, position: 'absolute', borderRadius: 5}} src="https://via.placeholder.com/328x184" />
-        <img style={{width: 328.47, height: 183.80, left: 1034.53, top: 0, position: 'absolute', borderRadius: 5}} src="https://via.placeholder.com/328x184" />
-        <div style={{width: 58.23, height: 35.49, padding: 10, left: 254.77, top: 135.57, position: 'absolute', background: '#1D55A8', borderRadius: 5, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
-            <div style={{color: 'white', fontSize: 16, fontFamily: 'Roboto', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Invest</div>
-        </div>
-        <div style={{width: 58.23, height: 35.49, padding: 10, left: 599.61, top: 135.57, position: 'absolute', background: '#1D55A8', borderRadius: 5, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
-            <div style={{color: 'white', fontSize: 16, fontFamily: 'Roboto', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Invest</div>
-        </div>
-        <div style={{width: 58.23, height: 35.49, padding: 10, left: 944.46, top: 135.57, position: 'absolute', background: '#1D55A8', borderRadius: 5, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
-            <div style={{color: 'white', fontSize: 16, fontFamily: 'Roboto', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Invest</div>
-        </div>
-        <div style={{width: 58.23, height: 35.49, padding: 10, left: 1289.30, top: 135.57, position: 'absolute', background: '#1D55A8', borderRadius: 5, justifyContent: 'center', alignItems: 'center', gap: 10, display: 'inline-flex'}}>
-            <div style={{color: 'white', fontSize: 16, fontFamily: 'Roboto', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Invest</div>
-        </div>
-    </div>
-    <div style={{width: 123, height: 0, left: 38, top: 612, position: 'absolute', border: '2px #616161 solid'}}></div>
-    <div style={{width: 45, height: 5, left: 45, top: 608, position: 'absolute', background: '#A5A5A5'}} />
-    <div style={{left: 45, top: 586, position: 'absolute', color: 'white', fontSize: 16, fontFamily: 'Roboto', fontWeight: '700', textTransform: 'capitalize', wordWrap: 'break-word'}}>Latest</div>
-    
-    <div style={{width: 392, left: 524, top: 213, position: 'absolute', color: '#1E1E1E', fontSize: 24, fontFamily: 'Roboto', fontWeight: '400', wordWrap: 'break-word'}}>Invest in the movies you want to see!</div>
-    <div style={{left: 546, top: 155, position: 'absolute', color: '#1E1E1E', fontSize: 45, fontFamily: 'Crimson Text', fontWeight: '700', textTransform: 'uppercase', wordWrap: 'break-word'}}>Company name</div>
-    <div style={{width: 53, height: 53, left: 38, top: 26, position: 'absolute', background: '#D9D9D9', borderRadius: 9999}} />
-</main>
-
+        <Carousel category="Most Invested" /> 
+        <Carousel category="Latest" /> 
+        <Carousel category="Features" /> 
+      </main>
     </>
   );
 }
