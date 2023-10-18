6f5e569b0d54b65d844f01f2679657c91b81fb22
Connor Wales <connorwales@gmail.com>
2023-10-18 13:25:17 -0700

    Change UserIcon to handle signIn and signOut, clean out example code

--- src/components/UserIcon/UserIcon.tsx
+++ src/components/UserIcon/UserIcon.tsx
@@ -12,39 +12,12 @@ const UserIcon = () => {
         className="rounded-full w-12 h-12 inline" 
         src={sessionData?.user?.image || "https://via.placeholder.com/48x48"}
       />
-      <span 
+      <button 
         className="text-white text-xl font-normal mx-4 my-auto"
-        // onClick={sessionData ? () => void null : () => void signIn}
+        onClick={sessionData ? () => void signOut() : () => void signIn()}
       >
-        {sessionData?.user?.name || "Sign In"}
-      </span>
+        {sessionData ? "Sign Out" : "Sign In" }
+      </button>
     </div>
   )
 }
-
-
-function AuthShowcase() {
-  const { data: sessionData } = useSession();
-
-  // const { data: secretMessage } = api.example.getSecretMessage.useQuery(
-  //   undefined, // no input
-  //   { enabled: sessionData?.user !== undefined }
-  // );
-
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {/* {secretMessage && <span> - {secretMessage}</span>} */}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => void signOut() : () => void signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-}
-
-export default UserIcon;
